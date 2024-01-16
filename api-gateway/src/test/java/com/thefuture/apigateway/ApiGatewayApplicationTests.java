c6b74adff2084eabc5fe36bc5280dc2bb5f3de9f
jeremyyonathan <jeremywib7@gmail.com>
2024-01-16 18:44:25 +0700

    Initial commit

---
+++ api-gateway/src/test/java/com/thefuture/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thefuture.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ naming-server/src/test/java/com/thefuture/namingserver/NamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thefuture.namingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
